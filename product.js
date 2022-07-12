cf2b313b413f41b9aa656580dd8124a9ba817dd5
Gareth-Ashton <garethlovesarchitects@gmail.com>
2022-07-12 10:22:37 +0200

    Adding new files

---
+++ product.js
@@ -0,0 +1,10 @@
+
+
+function change(){
+  if(document.getElementById("thumbnail2").src == "./img/Design3.jpg"){
+    document.getElementById("mainImage").src = "./img/Design3.jpg"
+  }
+else{
+  document.getElementById("mainImage").src = "./img/Design3.jpg"
+}}
+
