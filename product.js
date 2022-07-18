f6e531f333c8cf1ce8910cbeb3a4d789912183e1
Gareth Ashton <garethlovesarchitects@gmail.com>
2022-07-18 22:25:50 +0200

    Adding Hamburger Menu

--- product.js
+++ product.js
@@ -1,10 +1,16 @@
 
 
-function change(){
-  if(document.getElementById("thumbnail2").src == "./img/Design3.jpg"){
-    document.getElementById("mainImage").src = "./img/Design3.jpg"
-  }
-else{
-  document.getElementById("mainImage").src = "./img/Design3.jpg"
-}}
+// function change(){
+//   if(document.getElementById("thumbnail2").src == "./img/Design3.jpg"){
+//     document.getElementById("mainImage").src = "./img/Design3.jpg"
+//   }
+// else{
+//   document.getElementById("mainImage").src = "./img/Design3.jpg"
+// }}
 
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById("nav-ul");
+
+hamburger.addEventListener("click", () => {
+  navUL.classList.toggle('show');
+})
